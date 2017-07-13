baba808542b8d0a3c006bd5c67708faecf218e65
Rainer Sawitzki <training@rainer-sawitzki.de>
2017-07-13 10:02:10 +0100

    Digester3

--- org.javacream.training.maven.msg.simplejava/src/main/java/org/javacream/training/maven/msg/simplejava/Demo.java
+++ org.javacream.training.maven.msg.simplejava/src/main/java/org/javacream/training/maven/msg/simplejava/Demo.java
@@ -1,14 +1,19 @@
 package org.javacream.training.maven.msg.simplejava;
 
+import org.apache.commons.digester3.Digester;
 import org.apache.commons.lang3.SerializationUtils;
+
 
 public class Demo {
 
 	public void doSomething(){
 		System.out.println("Hello World!!!");
 	}
-	
+
 	public String lang3(){
 		return SerializationUtils.class.getName();
+	}
+	public String digester3(){
+		return Digester.class.getName();
 	}
 }
