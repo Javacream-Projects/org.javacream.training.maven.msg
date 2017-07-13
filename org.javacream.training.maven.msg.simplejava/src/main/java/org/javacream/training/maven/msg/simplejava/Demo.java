0b497af491745ad5f6c7de162bbedb5fd2e706d9
Rainer Sawitzki <training@rainer-sawitzki.de>
2017-07-13 12:23:09 +0100

    added javadoc

--- org.javacream.training.maven.msg.simplejava/src/main/java/org/javacream/training/maven/msg/simplejava/Demo.java
+++ org.javacream.training.maven.msg.simplejava/src/main/java/org/javacream/training/maven/msg/simplejava/Demo.java
@@ -3,13 +3,24 @@ package org.javacream.training.maven.msg.simplejava;
 import org.apache.commons.digester3.Digester;
 import org.apache.commons.lang3.SerializationUtils;
 
-
+/**
+ * Eine simple Demo-Klasse
+ * @author Rainer Sawitzki
+ *
+ */
 public class Demo {
 
+	/**
+	 * Eine einfache Demo-Funktion
+	 */
 	public void doSomething(){
 		System.out.println("Hello World!!!");
 	}
 
+	/**
+	 * 
+	 * @return Klassenname der lang3-Utility
+	 */
 	public String lang3(){
 		return SerializationUtils.class.getName();
 	}
