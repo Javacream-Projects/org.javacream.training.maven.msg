cfcefa624a47124f8faad8a8b4e175f85194bb35
Rainer Sawitzki <training@rainer-sawitzki.de>
2017-07-13 09:18:51 +0100

    Added Commons Lang3

--- org.javacream.training.maven.msg.simplejava/src/main/java/org/javacream/training/maven/msg/simplejava/Demo.java
+++ org.javacream.training.maven.msg.simplejava/src/main/java/org/javacream/training/maven/msg/simplejava/Demo.java
@@ -1,8 +1,14 @@
 package org.javacream.training.maven.msg.simplejava;
 
+import org.apache.commons.lang3.SerializationUtils;
+
 public class Demo {
 
 	public void doSomething(){
 		System.out.println("Hello World!!!");
+	}
+	
+	public String lang3(){
+		return SerializationUtils.class.getName();
 	}
 }
--- org.javacream.training.maven.msg.simplejava/src/test/java/org/javacream/training/maven/msg/simplejava/TestDemo.java
+++ org.javacream.training.maven.msg.simplejava/src/test/java/org/javacream/training/maven/msg/simplejava/TestDemo.java
@@ -1,5 +1,5 @@
 package org.javacream.training.maven.msg.simplejava;
 
 public class TestDemo {
-
+	
 }
