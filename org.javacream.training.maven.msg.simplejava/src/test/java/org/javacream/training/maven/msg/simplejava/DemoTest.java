50da49aee54d60d70bf346bf132f3a7d91006fa6
Rainer Sawitzki <training@rainer-sawitzki.de>
2017-07-13 12:19:43 +0100

    Fixed bug

--- org.javacream.training.maven.msg.simplejava/src/test/java/org/javacream/training/maven/msg/simplejava/DemoTest.java
+++ org.javacream.training.maven.msg.simplejava/src/test/java/org/javacream/training/maven/msg/simplejava/DemoTest.java
@@ -8,6 +8,6 @@ public class DemoTest {
 		Assert.assertSame("org.apache.commons.lang3.SerializationUtils", new Demo().lang3());
 	}
 	@Test public void testDigester3(){
-		Assert.assertSame("org.apache.commons.digester3.Digesterr", new Demo().digester3());
+		Assert.assertSame("org.apache.commons.digester3.Digester", new Demo().digester3());
 	}
 }
