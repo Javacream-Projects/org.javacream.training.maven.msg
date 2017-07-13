37f26606b2d97a752ac92667c82ae3912d842fc9
Rainer Sawitzki <training@rainer-sawitzki.de>
2017-07-13 13:48:21 +0100

    Created Project Java

---
+++ org.javacream.training.maven.msg.java/src/test/java/org/javacream/training/maven/msg/java/HugoTest.java
@@ -0,0 +1,11 @@
+package org.javacream.training.maven.msg.java;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HugoTest {
+
+	@Test public void testOk(){
+		Assert.assertTrue(true);
+	}
+}
